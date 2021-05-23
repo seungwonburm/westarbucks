453134748b1406105850e55b24ad15b965435887
seungwon <seungwon@bu.edu>
2021-05-24 03:27:37 +0900

    new commit

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table='menus'
+
+class Category(models.Model):
+    name= models.CharField(max_length=50)
+    menu = models.ForeignKey(Menu, on_delete= models.CASCADE)
+    class Meta:
+        db_table = 'products_category'
+
+class Drink(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    class Meta:
+        db_table = 'products_drink' 
+
+class Images(models.Model):
+    image_url= models.CharField(max_length=1000)
+    drink=models.ForeignKey(Drink, on_delete=models.CASCADE)
+
+class Allergy(models.Model):
+    name= models.CharField(max_length=50)
+    class Meta:
+        db_table='products_allergy'
+
+class AllergyDrink(models.Model):
+    allergy=models.ForeignKey(Allergy, on_delete=models.CASCADE)
+    drink=models.ForeignKey(Drink, on_delete=models.CASCADE, null = True, default='')
+    class Meta:
+        db_table='products_allergydrink'
